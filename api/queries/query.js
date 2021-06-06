15e37f52d5b6a1aa79927f264b3ad316bd9f456a
KingMob <john@onethreethree.net>
2021-06-06 21:42:35 +0100

    sort out the queries and env

--- api/queries/query.js
+++ api/queries/query.js
@@ -1,4 +1,7 @@
-const {Pool} = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {Pool,Client} = require('pg');
 const massive = require('massive');
 
 const dbConfig = {
@@ -9,8 +12,16 @@ const dbConfig = {
     port: process.env.PORT,
 }
 
-const pool = new Pool(dbConfig);
-const _db = massive(dbConfig);
+let pool,_db;
+
+if(process.env.NODE_ENV == 'production' ){
+    pool = new Client(process.env.DATABASE_URL);
+    _db = massive(process.env.DATABASE_URL);
+}
+else{
+    pool = new Pool(dbConfig);
+    _db = massive(dbConfig);
+}   
 
 module.exports = {
     pool,
