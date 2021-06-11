5700390591840d57af5a267daff993edea6df787
KingMob <john@onethreethree.net>
2021-06-11 19:04:37 +0100

    added title z-index

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -18,7 +18,9 @@ function App() {
     <Router>
       <div className="App">
            <header className="App-header">
-          <Link to="/"><h1 className="title">afternoonwideboys</h1></Link>
+          <Link to="/">
+            <h1 className="title">afternoonwideboys</h1>
+          </Link>
           <Switch>
             <Route path="/user/:userId">
               <User user={user} setUser={setUser} />
